95dc2c61a79a3f70a1a8d9e8f9e71f8ca782b295
Dirtburner <Dirtburner@users.noreply.github.com>
2021-08-21 23:58:20 -0400

    Fix bug in scraping routine that prevented advancing past page 20, added new datadump and analysis cells to notebook.

--- PCS_COVID_ScraPy.py
+++ PCS_COVID_ScraPy.py
@@ -36,7 +36,6 @@ def get_page_indices(driver):
     return paging_buttons, page_text_indices, page_numbers
 
 
-# https://stackoverflow.com/a/53073789/47078
 def initiate_scraping(url, driver_path):
     #Set up selenium web interaction -
     options = webdriver.ChromeOptions()
@@ -69,27 +68,40 @@ def determine_total_pages(url, driver_path):
     This function clicks the submit button, clicks on the ellipsis until the ellipsis is not the last
     in the list of page indices, and returns the total number of pages to be scraped of data tables.
     '''
-    #Click submit without filters so that all data are displayed in table over multiple pages
+
     driver = initiate_scraping(url, driver_path)
-    
-    for x in range(10000):      #This covers up to 10000 pages of data tables on the website.
+
+    for x in range(10000):
+        time.sleep(1.2)
         _, page_text_indices, page_numbers = get_page_indices(driver)
+        print('Iteration ', str(x), '.')
+        indices_numbers = list(zip(page_text_indices, page_numbers))
+        print('Last index/number - ' + str(len(indices_numbers)) +'**** ' + str(indices_numbers))
+    
         if page_text_indices[-1] == '...':
-            print('Iteration ', x, '. Clicking "..." to obtain pages after ', str(max(page_numbers)), ';\n searching for the last page.')
-            next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(max(page_numbers) + 1) + ']/a'
-            debug(next_page_xpath)
+            print('Page ' + str(max(page_numbers)) + ' is not the last. Clicking ellipsis to go to next.')
+            #Create xpath for ellipsis button:
+            if page_text_indices[0] == '...':
+                next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(len(page_text_indices)+2) +']/a'
+                debug(next_page_xpath)
+            else: 
+                next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(len(page_text_indices)) +']/a'
+                debug(next_page_xpath)
             button = driver.find_element_by_xpath(next_page_xpath)
+            time.sleep(1.2)
             driver.execute_script("arguments[0].click();", button)
         else:
-            print('Iteration ', x, 'No ellipsis (...) at the end of this page. Maximum obtained.')
+            print('Iteration ', x, 'No ellipsis (...) at the end of this page. Maximum obtained at page ', str(max(page_numbers)), '.')
             total_pages = max(page_numbers)
             break
+
     #Once determined, return the webpage to the original form with the submit button to execute
     #filterless search and scrape data:
     new_search_xpath = '//*[@id="module-content-64809"]/div/div[2]/ul/li/div/div[1]/span/span/p[1]/a'
     new_search_button = driver.find_element_by_xpath(new_search_xpath)
     driver.execute_script("arguments[0].click();", new_search_button)
 
+    
     return total_pages, page_text_indices, driver
 
 
@@ -138,7 +150,7 @@ def Scrape_data(url, driver_path, delay):
 
     #Loop through remaining pages, beginning with page 2
     for page in range(2, tot+1):
-        paging_buttons, page_text_indices, page_numbers = get_page_indices(driver)
+        _, _, page_numbers = get_page_indices(driver)
         print('Scraping page ' + str(page) + ' of ' + str(tot) + ' pages.')
         if page <= max(page_numbers):
             if first_loop is True: 
@@ -169,11 +181,18 @@ def Scrape_data(url, driver_path, delay):
         elif page > max(page_numbers):
             debug('Working from the elif loop (clicking the ellipsis).')
             #In this case, we click the right ellipsis.
-            next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(page) + ']/a'
-            debug('Page 1 iteration ' + str(page) + ', xpath = ' + str(next_page_xpath))
-            button = driver.find_element_by_xpath(next_page_xpath)
-            driver.execute_script("arguments[0].click();", button)
-            time.sleep(2)
+            if first_loop is True:
+                next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(page) + ']/a'
+                debug('Page 1 iteration ' + str(page) + ', xpath = ' + str(next_page_xpath))
+                button = driver.find_element_by_xpath(next_page_xpath)
+                driver.execute_script("arguments[0].click();", button)
+                time.sleep(2)
+            else:
+                next_page_xpath = '//*[@id="ui-paging-container"]/ul/li[' + str(13) + ']/a'
+                debug('Page 1 iteration ' + str(page) + ', xpath = ' + str(next_page_xpath))
+                button = driver.find_element_by_xpath(next_page_xpath)
+                driver.execute_script("arguments[0].click();", button)
+                time.sleep(2)
             #Scrape data and concatenate
             temp_df = get_table(driver)
             debug('Data scraped from page ' + str(page) + r' table ...\n, represented by "..."...')
--- dashboard_analysis.py
+++ dashboard_analysis.py
@@ -1,35 +1,37 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.11.4
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
+#     display_name: Python 3.9.1 64-bit
 #     name: python3
 # ---
 
+# %% [markdown]
 # # Analyze Dashboard Data for COVID Cases in Pinellas County Schools
 #
 # In this notebook, we analyze the data contained in Pinellas County School's COVID database. The tools include county wide totals, school by school analyses, and data visualization.
 #
 # This notebook analyzes the latest .csv data file scraped by the `dashboard_scrape.py` notebook. From this point on, we employ pandas and matplotlib to analyze and visualize the data. This notebook allows you to slice the data into various bins, by date, by school, by category, and shows some clever ways to plot the data compared to last year's data. 
 
+# %%
 #Load packages
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import date
 import matplotlib.dates as mdates
 
+# %% [markdown]
 # ## Data Analysis
 #
 # From this point on, we employ pandas and matplotlib to analyze and visualize the data. This notebook allows you to slice the data into various bins, by date, by school, by category, and shows some clever ways to plot the data compared to last year's data. 
 
-# +
+# %%
 # Import datadump.csv files
 
 # make a list of csvs
@@ -39,19 +41,22 @@ files = sorted(files, reverse=True)
 
 data_df = pd.read_csv(files[0])
 data_df
+print(data_df.columns)
+print(max(data_df['Date']))
 
-# +
+# %%
 #Uncomment the line below if the data have extra columns 0, 1, and 2 in the dataframe. This happens
 #sometimes when the scraping happens too fast for th table to load.
 #data_df.drop(columns=[0, 1, 2], inplace=True)
 
-# +
+# %%
 pcsb_students_2019 = 100000
 hcsb_students_2019 = 223305
 
 #Mask for cases since the beginning of the school year, 8/11/2021
-school_year_mask = data_df['Date']>='2021/08/11'
+school_year_mask = data_df['Date']>='2021-08-11'
 data_df = data_df.loc[school_year_mask]
+print(data_df.shape)
 
 #Add column for total cases, calculate grand total.
 data_df['Total cases'] = data_df['Number of positive employees'] + data_df['Number of positive students']
@@ -59,7 +64,7 @@ print(data_df.shape)
 print('Total cases in district = ' + str(sum(data_df['Total cases'].dropna())))
 total_cases_students = sum(data_df['Number of positive students'].dropna())
 print('Total covid+ student cases in district = ' + str(total_cases_students) + 
-      '(' + str(total_cases_students/pcsb_students_2019*100) + "%" + ' of students)')
+      ' (' + str(total_cases_students/pcsb_students_2019*100) + "%" + ' of students)')
 total_cases_employees = sum(data_df['Number of positive employees'].dropna())
 print('Total covid+ employees cases in district = ' + str(total_cases_employees))
 cases_by_date = data_df.groupby('Date').sum().sort_values(by='Date', ascending=True)
@@ -76,8 +81,8 @@ ax.legend(['Employees', 'Students'])
 
 plt.savefig('Daily Positive Cases.png')
 
-# -
 
+# %%
 #Cumulative sums
 school = 'Midtown Academy'
 data_df.sort_values(['Locations affected', 'Date'], ascending=[True, True], inplace=True)
@@ -89,11 +94,12 @@ fig, ax = plt.subplots()
 ax.plot(data_df['Date'].loc[data_df['Locations affected']==school], 
         data_df['Cumulative Sum Students'].loc[data_df['Locations affected']==school])
 
+# %% [markdown]
 # ## Focus on Midtown Academy (K-5, southside) vs. Dunedin (K-5, north county)
 #
 # To compare two schools, we select the text in the their names and use pandas to filter the rest of the data. For each one, a time series can be made for both students and employees who have tests positive for COVID 19 during this school year. These schools allow us to use geography as a proxy for how many people may become sick with COVID due to opening schools with no mask mandate and the highly contagious delta variant of COVID causing record daily positive cases and hospitalization in the county and across the state. 
 
-# +
+# %%
 #Use the following two lines to pick data from a single school:
 text_in_school1_name = 'Midtown'
 school1_df = data_df[data_df['Locations affected'].astype(str).str.contains(text_in_school1_name)].sort_values(by='Date', ascending=True)
@@ -128,5 +134,4 @@ ax.legend()
 #ax.xaxis.set_major_locator(fmt_weekly)
 #ax.xaxis.set_major_locator(mdates.YearLocator())
 #ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
-# -
-
+# %%
